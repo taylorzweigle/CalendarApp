0b8fb0eed6d322619f67b89d254e2337d5a54e5f
Taylor Zweigle <62271709+taylorzweigle@users.noreply.github.com>
2024-02-28 19:45:01 -0600

    started hiding routes when not signed in

---
+++ server/middleware/requireAuth.js
@@ -0,0 +1,26 @@
+//Taylor Zweigle, 2024
+const jwt = require("jsonwebtoken");
+
+const User = require("../models/userModel");
+
+const requireAuth = async (req, res, next) => {
+  const { authorization } = req.headers;
+
+  if (!authorization) {
+    return res.status(401).json({ error: "You must be logged in" });
+  }
+
+  const token = authorization.split(" ")[1];
+
+  try {
+    const { _id } = jwt.verify(token, process.env.JWT_SECRET);
+
+    req.user = await User.findOne({ _id }).select("_id");
+
+    next();
+  } catch (error) {
+    res.status(401).json({ error: "You must be logged in" });
+  }
+};
+
+module.exports = requireAuth;
--- server/routes/events.js
+++ server/routes/events.js
@@ -3,7 +3,11 @@ const express = require("express");
 
 const { getEvents, getEvent, createEvent, deleteEvent, updateEvent } = require("../controllers/eventController");
 
+const requireAuth = require("../middleware/requireAuth");
+
 const router = express.Router();
+
+router.use(requireAuth);
 
 router.get("/", getEvents);
 
