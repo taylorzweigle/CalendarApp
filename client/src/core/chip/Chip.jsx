69eaf2623de22fb69eff10f45eb84f502878178a
Taylor Zweigle <zweigletaylor@gmail.com>
2024-05-21 18:39:15 -0500

    hover state on chip

--- client/src/core/chip/Chip.jsx
+++ client/src/core/chip/Chip.jsx
@@ -6,7 +6,7 @@ const Chip = ({ selected, onClick, children }) => {
     <button
       className={`flex items-center justify-center rounded-lg w-20 h-12 ${
         selected
-          ? "bg-transparent text-slate-900 dark:text-white border-2 border-sky-600 dark:border-sky-400"
+          ? "bg-transparent active:bg-slate-50 active:dark:bg-slate-700 text-slate-900 dark:text-white border-2 border-sky-600 dark:border-sky-400"
           : "text-slate-700 dark:text-slate-300"
       }`}
       onClick={onClick}
