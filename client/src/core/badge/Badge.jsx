49d329c246399a3741508ec23aaa0ff688a39a01
Taylor Zweigle <zweigletaylor@gmail.com>
2024-05-23 20:30:27 -0500

    badge border

--- client/src/core/badge/Badge.jsx
+++ client/src/core/badge/Badge.jsx
@@ -23,7 +23,9 @@ const Badge = ({ size }) => {
   return (
     <span className={`absolute right-0 top-0 flex ${sizeClass} ${rippleClass}`}>
       <span className="animate-ping-slow absolute inline-flex h-full w-full rounded-full bg-rose-800 dark:bg-rose-200 opacity-75"></span>
-      <span className={`relative inline-flex rounded-full ${sizeClass} bg-rose-400 dark:bg-rose-500`}></span>
+      <span
+        className={`relative inline-flex rounded-full ${sizeClass} bg-rose-400 dark:bg-rose-500 border border-rose-800 dark:border-rose-200`}
+      ></span>
     </span>
   );
 };
