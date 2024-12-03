81e18b43c41d7a7fe608fbdc2ec99de6c5a4b401
Taylor Zweigle <zweigletaylor@gmail.com>
2024-12-02 19:53:47 -0600

    added toast, appears when todos are deleted

--- client/src/components/layouts/TodoLayout.jsx
+++ client/src/components/layouts/TodoLayout.jsx
@@ -19,6 +19,7 @@ import TodoCard from "../../components/cards/TodoCard";
 
 import { calendars } from "../../utility/calendars";
 import Divider from "../../core/divider/Divider";
+import Toast from "../../core/toast/Toast";
 
 const TodoLayout = ({ data }) => {
   const navigate = useNavigate();
@@ -28,6 +29,7 @@ const TodoLayout = ({ data }) => {
 
   const [todos, setTodos] = useState([]);
   const [dueTodos, setDueTodos] = useState([]);
+  const [deletedTodoCount, setDeletedTodoCount] = useState(0);
 
   const [loading, setLoading] = useState("");
 
@@ -37,6 +39,8 @@ const TodoLayout = ({ data }) => {
   }, [data]);
 
   const handleRefreshClick = async () => {
+    let deleteCount = 0;
+
     const deleteCheckedTodo = async (id) => {
       const deletedTodo = await deleteTodo(id, authUser.token);
 
@@ -50,12 +54,15 @@ const TodoLayout = ({ data }) => {
         if (
           todos[i].checked &&
           todos[i].checkedTime &&
-          new Date().getTime() - new Date(todos[i].checkedTime).getTime() > 43200000
+          new Date().getTime() - new Date(todos[i].checkedTime).getTime() > 10
         ) {
           deleteCheckedTodo(todos[i]._id);
+          deleteCount++;
         }
       }
     }
+
+    setDeletedTodoCount(deleteCount);
   };
 
   const handleCheck = async (todo) => {
@@ -85,72 +92,77 @@ const TodoLayout = ({ data }) => {
   const showBadge = (time) => new Date().getTime() - new Date(time).getTime() < 43200000;
 
   return (
-    <div className="min-h-[calc(100vh-412px)] md:min-h-[calc(100vh-168px)]">
-      <div className="flex flex-row justify-between items-center p-4 md:p-0">
-        <Typography variant="title">Todos</Typography>
-        <Button prefix={<CachedIcon />} onClick={handleRefreshClick} />
-      </div>
-      <Divider />
-      <div className="flex flex-col gap-4 md:gap-8 p-4 md:p-0">
-        {dueTodos.length > 0 && (
+    <>
+      <Toast open={deletedTodoCount > 0}>{`${deletedTodoCount} Todo${
+        deletedTodoCount > 1 ? "s" : ""
+      } successfully deleted`}</Toast>
+      <div className="min-h-[calc(100vh-412px)] md:min-h-[calc(100vh-168px)]">
+        <div className="flex flex-row justify-between items-center p-4 md:p-0">
+          <Typography variant="title">Todos</Typography>
+          <Button prefix={<CachedIcon />} onClick={handleRefreshClick} />
+        </div>
+        <Divider />
+        <div className="flex flex-col gap-4 md:gap-8 p-4 md:p-0">
+          {dueTodos.length > 0 && (
+            <div className="flex flex-col gap-4">
+              <Typography variant="body2" color="secondary">
+                DUE TODAY
+              </Typography>
+              <div className="flex flex-col gap-4">
+                {dueTodos.map((todo) => (
+                  <TodoCard
+                    key={todo._id}
+                    todo={todo.todo}
+                    color={calendars.find((calendar) => calendar.user === todo.user).color}
+                    dueDate={todo.dueDate}
+                    notes={todo.notes}
+                    checked={todo.checked}
+                    loading={loading === todo._id}
+                    badge={showBadge(todo.creationTime)}
+                    onClick={() => handleEdit(todo._id)}
+                    onCheck={() => handleCheck(todo)}
+                  />
+                ))}
+              </div>
+            </div>
+          )}
           <div className="flex flex-col gap-4">
-            <Typography variant="body2" color="secondary">
-              DUE TODAY
-            </Typography>
-            <div className="flex flex-col gap-4">
-              {dueTodos.map((todo) => (
-                <TodoCard
-                  key={todo._id}
-                  todo={todo.todo}
-                  color={calendars.find((calendar) => calendar.user === todo.user).color}
-                  dueDate={todo.dueDate}
-                  notes={todo.notes}
-                  checked={todo.checked}
-                  loading={loading === todo._id}
-                  badge={showBadge(todo.creationTime)}
-                  onClick={() => handleEdit(todo._id)}
-                  onCheck={() => handleCheck(todo)}
-                />
-              ))}
-            </div>
+            {dueTodos.length > 0 && todos.length > 0 && (
+              <Typography variant="body2" color="secondary">
+                ALL TODOS
+              </Typography>
+            )}
+            {dueTodos.length > 0 && todos.length === 0 && (
+              <Typography variant="body2" color="secondary">
+                ALL TODOS
+              </Typography>
+            )}
+            {todos.length > 0 ? (
+              <div className="flex flex-col gap-4">
+                {todos.map((todo) => (
+                  <TodoCard
+                    key={todo._id}
+                    todo={todo.todo}
+                    color={calendars.find((calendar) => calendar.user === todo.user).color}
+                    dueDate={todo.dueDate}
+                    notes={todo.notes}
+                    checked={todo.checked}
+                    loading={loading === todo._id}
+                    badge={showBadge(todo.creationTime)}
+                    onClick={() => handleEdit(todo._id)}
+                    onCheck={() => handleCheck(todo)}
+                  />
+                ))}
+              </div>
+            ) : (
+              <div className="pt-8">
+                <EmptyState type="Todo" />
+              </div>
+            )}
           </div>
-        )}
-        <div className="flex flex-col gap-4">
-          {dueTodos.length > 0 && todos.length > 0 && (
-            <Typography variant="body2" color="secondary">
-              ALL TODOS
-            </Typography>
-          )}
-          {dueTodos.length > 0 && todos.length === 0 && (
-            <Typography variant="body2" color="secondary">
-              ALL TODOS
-            </Typography>
-          )}
-          {todos.length > 0 ? (
-            <div className="flex flex-col gap-4">
-              {todos.map((todo) => (
-                <TodoCard
-                  key={todo._id}
-                  todo={todo.todo}
-                  color={calendars.find((calendar) => calendar.user === todo.user).color}
-                  dueDate={todo.dueDate}
-                  notes={todo.notes}
-                  checked={todo.checked}
-                  loading={loading === todo._id}
-                  badge={showBadge(todo.creationTime)}
-                  onClick={() => handleEdit(todo._id)}
-                  onCheck={() => handleCheck(todo)}
-                />
-              ))}
-            </div>
-          ) : (
-            <div className="pt-8">
-              <EmptyState type="Todo" />
-            </div>
-          )}
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
---
+++ client/src/core/toast/Toast.jsx
@@ -0,0 +1,40 @@
+//Taylor Zweigle, 2024
+import React, { useState, useEffect } from "react";
+
+import CloseIcon from "@mui/icons-material/Close";
+
+import Typography from "../typography/Typography";
+import IconButton from "../iconButton/IconButton";
+
+const Toast = ({ children, open }) => {
+  const [visible, setVisible] = useState(false);
+
+  useEffect(() => {
+    if (open) {
+      setVisible(true);
+
+      setTimeout(() => {
+        setVisible(false);
+      }, 2500);
+    }
+
+    return () => {
+      setVisible(false);
+    };
+  }, [open]);
+
+  return (
+    <div className={`${visible ? "fixed" : "hidden"} flex justify-center z-50 bottom-0 h-fit w-full`}>
+      <div className="flex justify-between items-center bg-slate-300 dark:bg-slate-500 w-full md:w-96 h-14 pl-4">
+        <Typography variant="body1" color="primary">
+          {children}
+        </Typography>
+        <IconButton color="default" onClick={() => setVisible(false)}>
+          <CloseIcon />
+        </IconButton>
+      </div>
+    </div>
+  );
+};
+
+export default Toast;
