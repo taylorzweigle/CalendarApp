4a443fbb3cab9c47466b79ad7506e8ce8e745f98
Taylor Zweigle <zweigletaylor@gmail.com>
2024-12-02 20:38:35 -0600

    fixed light theme area text input colors

--- client/src/core/areaTextInput/AreaTextInput.jsx
+++ client/src/core/areaTextInput/AreaTextInput.jsx
@@ -18,7 +18,7 @@ const AreaTextInput = forwardRef(({ label, value, rows, maxLength, error, showLa
         rows={rows}
         maxLength={maxLength}
         onChange={onChange}
-        className="bg-white dark:bg-slate-950 border-2 border-slate-400 dark:border-slate-600 sm:hover:border-sky-500 sm:hover:dark:border-sky-600 text-slate-950 dark:text-white text-md w-full rounded-lg p-4 resize-none"
+        className="bg-white dark:bg-slate-950 border-2 border-slate-300 dark:border-slate-600 sm:hover:border-sky-500 sm:hover:dark:border-sky-600 text-slate-950 dark:text-white text-md w-full rounded-lg p-4 resize-none"
       />
       {error && (
         <Typography variant="body2" color="error">
