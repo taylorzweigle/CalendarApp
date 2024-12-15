9f48e81ed00ba0c187008c66c8b512628768ba1b
Taylor Zweigle <zweigletaylor@gmail.com>
2024-12-15 14:25:09 -0600

    attempted fix to get safari select to style correctly

--- client/src/core/selectInput/SelectInput.jsx
+++ client/src/core/selectInput/SelectInput.jsx
@@ -27,7 +27,7 @@ const SelectInput = forwardRef(({ label, value, showIcon, error, items, showLabe
           ref={ref}
           value={value}
           onChange={onChange}
-          className={`bg-white dark:bg-slate-950 border-2 border-slate-300 dark:border-slate-600 sm:hover:border-sky-500 sm:hover:dark:border-sky-600 text-slate-950 dark:text-white text-md w-full rounded-lg ${
+          className={`bg-white dark:bg-slate-950 border-2 border-slate-300 dark:border-slate-600 sm:hover:border-sky-500 sm:hover:dark:border-sky-600 text-slate-950 dark:text-white text-md text-left w-full rounded-lg ${
             showIcon ? "indent-5" : "indent-2"
           } h-12 w-full`}
         >
