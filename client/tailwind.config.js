1ea0194b5cd76dba1d18b0a48355fdd2a96f56c1
Taylor Zweigle <zweigletaylor@gmail.com>
2025-01-03 18:29:16 -0600

    added dynamic styles to tailwind config safelist

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,6 +2,13 @@
 
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  safelist: [
+    {
+      pattern:
+        /(bg|hover:bg|active:bg|border|text|icon)-(slate|gray|zinc|neutral|stone|red|orange|amber|yellow|lime|green|emerald|teal|cyan|sky|blue|indigo|violet|purple|fuchsia|pink|rose)-(100|200|300|400|500|600|700|800|900|950)/,
+      variants: ["dark", "hover", "active"],
+    },
+  ],
   theme: {
     extend: {
       borderWidth: {
